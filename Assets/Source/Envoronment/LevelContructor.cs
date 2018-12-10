04cea35200c012fab99c0215cc425cf61988568b
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2018-12-10 03:02:08 +0100

    Level constructor and destroyer

---
+++ Assets/Source/Envoronment/LevelContructor.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ConstructType
+{
+    Undecided,
+    Floor,
+    Hole,
+    Block,
+    FloatBlock
+}
+
+public class LevelContructor : MonoBehaviour {
+
+    public float constructDistance = 50;
+    public float constructAcceptance = 10;
+    public float constructEveryDistance = 3;
+
+    public GameObject floor;
+    public GameObject block;
+
+    public float floorChance = .6f;
+    public float holeChance = .2f;
+    public float blockChance = .1f;
+    public float floatBlockChance = .1f;
+
+    public float blockHeight = .5f;
+    public float floatBlockHeight = 1;
+
+    
+    private ConstructType constructType = ConstructType.Undecided;
+    private GameObject playerCharacterObj;
+    private float preConstructZ = 0;
+
+	void Start ()
+    {
+        playerCharacterObj = GameObject.FindGameObjectWithTag("Player");
+        preConstructZ = playerCharacterObj.transform.position.z + constructDistance;
+    }
+
+    void Update ()
+    {
+        float targetZ = playerCharacterObj.transform.position.z + constructDistance;
+
+        MoveZTo(targetZ);
+
+        if (ReachNextConstructionPosition(targetZ))
+        {
+            if (constructType == ConstructType.Undecided) { DecideContructionType(); }
+            else { Construct(); }
+        }
+
+    }
+
+    private void DecideContructionType()
+    {
+        float dice = Random.Range(0.0f, 1.0f);
+        if (0 <= dice && dice < floorChance) { constructType = ConstructType.Floor; }
+        else if (dice < floorChance + holeChance) { constructType = ConstructType.Hole; }
+        else if (dice < floorChance + holeChance + blockChance) { constructType = ConstructType.Block; }
+        else if (dice < floorChance + holeChance + floatBlockChance) { constructType = ConstructType.FloatBlock; }
+        Debug.Log(constructType);
+    }
+
+    private void Construct()
+    {
+        if(floor == null || block == null) { return; }
+        switch (constructType)
+        {
+            case (ConstructType.Floor):
+                ConstructFloor();
+                break;
+            case (ConstructType.Block):
+                ConstructFloor();
+                // ConstructBlock();
+                break;
+            default:
+                break;
+        }
+
+        preConstructZ = transform.position.z;
+        constructType = ConstructType.Undecided;
+    }
+
+    private void MoveZTo(float targetZ)
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, targetZ);
+    }
+
+    private bool ReachNextConstructionPosition(float targetZ)
+    {
+        float constructZ = preConstructZ + constructEveryDistance;
+        if(Mathf.Abs(targetZ - constructZ) < constructAcceptance)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, constructZ);
+            return true;
+        }
+        return false;
+    }
+
+    private void ConstructFloor() { GameObject.Instantiate(floor, transform.position, transform.rotation); }
+    private void ConstructBlock() { GameObject.Instantiate(block, transform.position + transform.up * blockHeight, transform.rotation); }
+}
---
+++ Assets/Source/Envoronment/LevelDestroyer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(BoxCollider))]
+public class LevelDestroyer : MonoBehaviour {
+
+    public float destroyDistanceBehind = 0f;
+    GameObject mainCamera;
+
+	void Start ()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+
+    }
+
+    void Update ()
+    {
+        transform.position = mainCamera.transform.position - transform.forward * ((GetComponent<BoxCollider>().size.z/2) + destroyDistanceBehind);
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        Vector3 selfCentre = transform.position;
+        float selfLength = transform.localScale.z * GetComponent<BoxCollider>().size.z / 2;
+
+        Vector3 otherCentre = other.gameObject.GetComponent<Renderer>().bounds.center;
+        float otherLength = other.gameObject.GetComponent<Renderer>().bounds.extents.magnitude ;
+
+        if (GetFrontCentrePosition(selfCentre, selfLength).z > GetFrontCentrePosition(otherCentre, otherLength).z) { Destroy(other.gameObject); }
+    }
+
+    private Vector3 GetFrontCentrePosition(Vector3 centrePosition, float length)
+    {
+        return centrePosition + transform.forward * length;
+    }
+
+}
